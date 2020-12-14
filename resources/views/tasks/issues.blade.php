a0c501e095584d460a5682296a53185946a435fd
Hiroya Haraguni <hiroya0100@gmail.com>
2020-12-14 21:35:31 +0900

    前コミットのバグ解消

--- resources/views/tasks/issues.blade.php
+++ resources/views/tasks/issues.blade.php
@@ -61,7 +61,7 @@
                                             <span class="badge badge-secondary">教師ユーザー</span>
                                         @endif
                                     </div>
-                                    {{!! nl2br(e($issue->statement)) !!}}
+                                    {!! nl2br(e($issue->statement)) !!}
                                 </div>
                             </div>
                         @endforeach
